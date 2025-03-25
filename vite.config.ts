2b023ad82dea17e0b52abc9f1e75a9e4ecb7ee93
fulsun <fulsun@xx.com>
2025-03-25 21:32:29 +0800

    refactor(vite): 配置Vite服务器代理并简化App.vue组件
    - 在vite.config.ts中添加服务器代理配置以解决跨域问题
    - 删除HelloWorld.vue组件并简化App.vue组件，仅保留基本功能
    - 添加pnpm-lock.yaml文件以锁定依赖版本

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+    // 代理解决跨域问题
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9527',
+        changeOrigin: true,
+        // rewrite: (path: string) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
